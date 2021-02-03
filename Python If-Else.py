fd4281f1a21b24457ed25ccd951082f653a66763
VGalkin <galkinvladimir2021@gmail.com>
2021-02-03 10:58:42 +0300

    https://www.hackerrank.com/challenges/py-if-else/problem

---
+++ Python If-Else.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    print("Введите число:")
+    n = int(input().strip())
+
+if n % 2 != 0:              #проверка на чётное
+    print("Weird")
+else:
+    if n > 100:
+        exit(0)
+    elif n >= 2 and n <= 5:
+        print("Not Weird")
+    elif n >= 6 and n <= 20:
+        print("Weird")
+    elif n > 20:
+        print("Not Weird")
